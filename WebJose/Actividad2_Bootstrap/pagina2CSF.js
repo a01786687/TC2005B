117f7a6884783e08be4681f17eb3fb89d77a88fa
renata.tec <a01786687@tec.mx>
2026-02-26 22:31:55 -0600

    continuación de actividad en clase con el profesor

--- WebJose/Actividad2_Bootstrap/pagina2CSF.js
+++ WebJose/Actividad2_Bootstrap/pagina2CSF.js
@@ -1,3 +1,36 @@
 $(document).ready(function () {
-    $("#tamaid").slide();
-})
+
+    // funcion para que el video haga slide down
+    $(".row").hide();
+    $(".row").slideDown("slow");
+
+    let control = 0; // variable para contar cuantos clicks le di al boton
+    $("#opcion1").click(function () {
+        $("#tamaid").fadeOut();
+        if (control == 0) { // secuencia de control
+            $("#tamaid").fadeOut();
+            control++;
+
+        }
+        else if (control == 1) {
+            $("#tamaid").fadeIn();
+            control++;
+        }
+
+        else if (control == 2) {
+            $("#tamaid").hide();
+            control++;
+        }
+
+        else {
+            $("#tamaid").show();
+            control = 0; // control 0 para que comienze el contador de nuevo
+        }
+
+    });
+
+});
+
+// # -> id
+// . -> clases
+// nada -> etiquetas
